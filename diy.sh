1380ba11ca96e69efbc0321e8372e283e9727fd8
ljhnchina <jiandjh@qq.com>
2021-03-06 13:04:11 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,35 +1,49 @@
 #!/usr/bin/env bash
-#添加hosts;如无法正常下载Github Raw文件，请注释掉
-#Host_IP=('151.101.88.133' '151.101.228.133')
-#Host_Name=('raw.githubusercontent.com' 'raw.githubusercontent.com')
-#for (( i=0; i<=${#Host_IP[@]}; i++ )) do
-#echo "${Host_IP[$i]} ${Host_Name[$i]}" >> /etc/hosts
-#done
 
-##############################作者昵称（必填）##############################
+# 把此diy.sh放入config即可,会自动同步最新脚本
+# 如有好用的脚本或者脚本更新不及时请@qiao112
+# 2021年3月5日23:00
+
+############################## 作者昵称 ##############################
 # 使用空格隔开
-author_list="ljhnchina qq34347476 ZCY01 i-chenzhe"
+author_list="Tartarus2014 i-chenzhe whyour moposmall qq34347476 ZCY01 shuye72"
 
-##############################作者脚本地址URL（必填）##############################
+############################## 脚本地址 ##############################
 # 例如：https://raw.githubusercontent.com/whyour/hundun/master/quanx/jx_nc.js
 # 1.从作者库中随意挑选一个脚本地址，每个作者的地址添加一个即可，无须重复添加
 # 2.将地址最后的 “脚本名称+后缀” 剪切到下一个变量里（my_scripts_list_xxx）
-# scripts_base_url_1=https://gitee.com/qq34347476/quantumult-x/raw/master/
-scripts_base_url_1=https://raw.githubusercontent.com/ljhnchina/jd_script/scripts/
-scripts_base_url_2=https://raw.githubusercontent.com/qq34347476/js_script/master/scripts/
-scripts_base_url_3=https://raw.githubusercontent.com/ZCY01/daily_scripts/main/jd/
-scripts_base_url_4=https://raw.githubusercontent.com/i-chenzhe/qx/main/
+scripts_base_url_1=https://ghproxy.com/https://raw.githubusercontent.com/Tartarus2014/Script/master/
+scripts_base_url_2=https://ghproxy.com/https://raw.githubusercontent.com/i-chenzhe/qx/main/
+scripts_base_url_3=https://ghproxy.com/https://raw.githubusercontent.com/whyour/hundun/master/quanx/
+scripts_base_url_4=https://ghproxy.com/https://raw.githubusercontent.com/moposmall/Script/main/Me/
+scripts_base_url_5=https://ghproxy.com/https://raw.githubusercontent.com/qq34347476/js_script/master/scripts/
+scripts_base_url_6=https://ghproxy.com/https://raw.githubusercontent.com/ZCY01/daily_scripts/main/jd/
+scripts_base_url_7=https://gitee.com/shuye72/MyActions/raw/main/
 
-
-##############################作者脚本名称（必填）##############################
+############################## 脚本名称 ##############################
 # 将相应作者的脚本填写到以下变量中
-my_scripts_list_1="xiaomi-step diy_shopsign"
-my_scripts_list_2="format_share_jd_code.js jd_try.js jd_joy_run.js"
-my_scripts_list_3="jd_priceProtect.js"
-my_scripts_list_4="jd_getFanslove.js jd_fanslove.js jd_gjmh.js jd_entertainment.js jd_shake.js jd_shakeBean.js jd_xmf.js"
+# 维护:Tartarus2014 库地址:https://github.com/Tartarus2014/Script
+my_scripts_list_1=""
 
+# 维护:i-chenzhe   库地址:https://github.com/i-chenzhe/qx
+my_scripts_list_2="jd_entertainment.js jd_fanslove.js jd_gjmh.js jd_mlyjy.js jd_shake.js jd_shakeBean.js jd_jump-jump.js jd_xmf.js z_marketLottery.js"
 
-##############################随机函数##########################################
+# 维护:whyour      库地址:https://github.com/whyour/hundun/tree/master/quanx
+my_scripts_list_3="jd_zjd_tuan.js"
+
+# 维护:moposmall   库地址:https://github.com/moposmall/Script/tree/main/Me
+my_scripts_list_4="jx_cfd_exchange.js"
+
+# 维护:qq34347476  库地址:https://github.com/qq34347476/js_script
+my_scripts_list_5="format_share_jd_code.js"
+
+# 维护:ZCY01       库地址:https://github.com/ZCY01/daily_scripts/tree/main/jd
+my_scripts_list_6="jd_priceProtect.js"
+
+# 维护:shuye72     库地址:https://gitee.com/shuye72/MyActions/tree/main ps:尽量不要使用此人脚本,
+my_scripts_list_7=""
+
+############################## 随机函数 ##############################
 rand(){
     min=$1
     max=$(($2-$min+1))
@@ -37,27 +51,18 @@ rand(){
     echo $(($num%$max+$min))
 }
 
-
-
+############################## 下载脚本 ##############################
 cd $ScriptsDir
-# 清理过期脚本
-rm -rf ZCY01_jd_try.js
-rm -rf i-chenzhe_jd_asus_iqiyi.js
-
-# 下载脚本
 index=1
 for author in $author_list
 do
-  echo -e "开始下载 $author 的脚本"
+  echo -e "############################## 开始下载 $author 的脚本 ##############################"
   # 下载my_scripts_list中的每个js文件，重命名增加前缀"作者昵称_"，增加后缀".new"
   eval scripts_list=\$my_scripts_list_${index}
-  echo $scripts_list
   eval url_list=\$scripts_base_url_${index}
-  echo $url_list
   for js in $scripts_list
   do
     eval url=$url_list$js
-    echo $url
     eval name=$author"_"$js
     echo $name
     wget -q --no-check-certificate $url -O $name.new
@@ -72,9 +77,9 @@ do
 	  if [ -z "${script_date}" ];then
 	    cron_min=$(rand 1 59)
 	    cron_hour=$(rand 7 9)
-	    [ $(grep -c "$croname" /jd/config/crontab.list) -eq 0 ] && sed -i "/hangup/a${cron_min} ${cron_hour} * * * bash jd $croname"  /jd/config/crontab.list
+	    [ $(grep -c "$croname" ${ConfigDir}/crontab.list) -eq 0 ] && sed -i "/hangup/a${cron_min} ${cron_hour} * * * bash jd $croname"  ${ConfigDir}/crontab.list
 	  else
-	    [ $(grep -c "$croname" /jd/config/crontab.list) -eq 0 ] && sed -i "/hangup/a${script_date} bash jd $croname"  /jd/config/crontab.list
+	    [ $(grep -c "$croname" ${ConfigDir}/crontab.list) -eq 0 ] && sed -i "/hangup/a${script_date} bash jd $croname"  ${ConfigDir}/crontab.list
 	  fi
     else
       [ -f $name.new ] && rm -f $name.new
@@ -84,26 +89,28 @@ do
   index=$[$index+1]
 done
 
-##############################同步 diy.sh ##########################################
+############################## 修改更新频率 ##############################
+echo -e "开始修改更新时间"
+if [ -f ${ListCron} ]; then
+  cron_min=$(rand 1 30) 
+  perl -i -pe "s|.+(bash git_pull.+)|${cron_min} \* \* \* \* \1|" ${ListCron}
+  crontab ${ListCron}
+  echo -e "修改更新时间成功"
+else
+  echo -e "修改更新时间失败"
+fi
+
+############################## 更新群助力脚本 ##############################
+bash ${ConfigDir}/sharecode.sh
+
+############################## 更新diy.sh ##############################
 cd $ConfigDir
 echo -e "开始更新 diy.sh "
-wget -q --no-check-certificate https://raw.githubusercontent.com/ljhnchina/jd_script/blob/master/diy.sh -O diy.sh.new
+wget -q --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/Hydrahail-Johnson/diy_scripts/main/diy.sh -O diy.sh.new
 if [ $? -eq 0 ]; then
   mv -f diy.sh.new diy.sh
   echo -e "更新 diy.sh 完成"
 else
   rm -rf diy.sh.new
   echo -e "更新 diy.sh 失败，使用上一次正常的版本...\n"
-fi
-
-##############################同步 manual-update.sh ##########################################
-cd $ShellDir
-echo -e "开始更新 manual-update.sh "
-wget -q --no-check-certificate https://raw.githubusercontent.com/qq34347476/js_script/master/scripts/manual-update.sh -O manual-update.sh.new
-if [ $? -eq 0 ]; then
-  mv -f manual-update.sh.new manual-update.sh
-  echo -e "更新 manual-update.sh 完成"
-else
-  rm -rf manual-update.sh.new
-  echo -e "更新 manual-update.sh 失败，使用上一次正常的版本...\n"
 fi
