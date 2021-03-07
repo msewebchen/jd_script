ac65d83756eeaf394632817d2ebafe8e45e8291b
ljhnchina <jiandjh@qq.com>
2021-03-07 22:35:35 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -54,29 +54,6 @@ rand(){
     echo $(($num%$max+$min))
 }
 
-############################## 净化脚本内置助力 ##############################
-function clearCode {
-    for file in $SCRIPTS_DIR/*.js; do
-        LINES=($(sed -n '/将采纳本脚本自带的助力码/=' $file))
-        if [ -n "$LINES" ] ; then
-        for i in `seq  ${#LINES[*]} -1 1`; do
-            LINE=${LINES[i-1]}
-            LAST_LINE=($(sed -n "$[$LINE+1],$[$LINE+10]{/}/=}" $file))
-            sed -i "$LINE,$[${LAST_LINE[0]}-1]d" $file
-            AU_URL="https://gitee.com/shylocks/updateTeam"
-            sed -i "s|https://gitee.com/shylocks/updateTeam|https://www.baidu.com|" $file
-            sed -i "s|https://gitee.com/lxk0301/updateTeam|https://www.baidu.com|" $file
-        done
-        fi
-    done
-    echo "净化脚本,人人有责"
-    
-    echo -e "开始去除通知小尾巴"
-    CODE_LINE=$(sed -n '/本脚本开源免费使用/=' sendNotify.js| head -1)
-    if [ -n "$CODE_LINE" ] ; then
-        sed -i "$CODE_LINE d" sendNotify.js
-    fi
-}
 ############################## 下载脚本 ##############################
 cd $ScriptsDir
 index=1
