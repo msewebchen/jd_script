44a897d9044bf934ce0c49565488d25c4e4a7b25
ljhnchina <jiandjh@qq.com>
2021-03-07 22:22:40 +0800

    Add files via upload

--- diy.sh
+++ diy.sh
@@ -1,39 +1,111 @@
-SCRIPTS_DIR="$JD_DIR/scripts"
-## 净化脚本内置助力
-function clearCode {
-    for file in $SCRIPTS_DIR/*.js; do
-        LINES=($(sed -n '/将采纳本脚本自带的助力码/=' $file))
-        if [ -n "$LINES" ] ; then
-        for i in `seq  ${#LINES[*]} -1 1`; do
-            LINE=${LINES[i-1]}
-            LAST_LINE=($(sed -n "$[$LINE+1],$[$LINE+10]{/}/=}" $file))
-            sed -i "$LINE,$[${LAST_LINE[0]}-1]d" $file
-            AU_URL="https://gitee.com/shylocks/updateTeam"
-            sed -i "s|https://gitee.com/shylocks/updateTeam|https://www.baidu.com|" $file
-            sed -i "s|https://gitee.com/lxk0301/updateTeam|https://www.baidu.com|" $file
-        done
-        fi
-    done
-    echo "净化脚本,人人有责"
-    
-    echo -e "开始去除通知小尾巴"
-    CODE_LINE=$(sed -n '/本脚本开源免费使用/=' sendNotify.js| head -1)
-    if [ -n "$CODE_LINE" ] ; then
-        sed -i "$CODE_LINE d" sendNotify.js
-    fi
+#!/usr/bin/env bash
+# 把此diy.sh放入config即可,会自动同步最新脚本
+# 如有好用的脚本或者脚本更新不及时请@ljhnchina
+# 2021年3月7日 16:38:58
+
+############################## 作者昵称 ##############################
+# 使用空格隔开
+author_list="ljhnchina Tartarus2014 i-chenzhe whyour moposmall qq34347476 ZCY01 shuye72"
+
+############################## 脚本地址 ##############################
+# 例如：https://raw.githubusercontent.com/whyour/hundun/master/quanx/jx_nc.js
+# 1.从作者库中随意挑选一个脚本地址，每个作者的地址添加一个即可，无须重复添加
+# 2.将地址最后的 “脚本名称+后缀” 剪切到下一个变量里（my_scripts_list_xxx）
+scripts_base_url_1=https://ghproxy.com/https://raw.githubusercontent.com/ljhnchina/jd_script/master/
+scripts_base_url_2=https://ghproxy.com/https://raw.githubusercontent.com/Tartarus2014/Script/master/
+scripts_base_url_3=https://ghproxy.com/https://raw.githubusercontent.com/i-chenzhe/qx/main/
+scripts_base_url_4=https://ghproxy.com/https://raw.githubusercontent.com/whyour/hundun/master/quanx/
+scripts_base_url_5=https://ghproxy.com/https://raw.githubusercontent.com/moposmall/Script/main/Me/
+scripts_base_url_6=https://ghproxy.com/https://raw.githubusercontent.com/qq34347476/js_script/master/scripts/
+scripts_base_url_7=https://ghproxy.com/https://raw.githubusercontent.com/ZCY01/daily_scripts/main/jd/
+scripts_base_url_8=https://gitee.com/shuye72/MyActions/raw/main/
+
+############################## 脚本名称 ##############################
+# 将相应作者的脚本填写到以下变量中
+# 维护:ljhnchina 库地址:https://raw.githubusercontent.com/ljhnchina/jd_script/
+my_scripts_list_1="diy_shopsign.js jd_crazy_joy_compose.js jd_firecrackers.js jd_submit_code.js jx_cfd.js jx_cfdtx.js jd_daydlt.js"
+
+# 维护:Tartarus2014 库地址:https://github.com/Tartarus2014/Script
+my_scripts_list_2=""
+
+# 维护:i-chenzhe   库地址:https://github.com/i-chenzhe/qx
+my_scripts_list_3="jd_entertainment.js jd_fanslove.js jd_gjmh.js jd_mlyjy.js jd_shake.js jd_shakeBean.js jd_xmf.js z_marketLottery.js"
+
+# 维护:whyour      库地址:https://github.com/whyour/hundun/tree/master/quanx
+my_scripts_list_4=""
+
+# 维护:moposmall   库地址:https://github.com/moposmall/Script/tree/main/Me
+my_scripts_list_5="jx_cfd_exchange.js"
+
+# 维护:qq34347476  库地址:https://github.com/qq34347476/js_script
+my_scripts_list_6="format_share_jd_code.js"
+
+# 维护:ZCY01       库地址:https://github.com/ZCY01/daily_scripts/tree/main/jd
+my_scripts_list_7="jd_priceProtect.js"
+
+# 维护:shuye72     库地址:https://gitee.com/shuye72/MyActions/tree/main ps:尽量不要使用此人脚本,
+my_scripts_list_8=""
+
+############################## 随机函数 ##############################
+rand(){
+    min=$1
+    max=$(($2-$min+1))
+    num=$(cat /proc/sys/kernel/random/uuid | cksum | awk -F ' ' '{print $1}')
+    echo $(($num%$max+$min))
 }
 
-function updateCodeUrl {
-    sed -i "s|http://jd.turinglabs.net/api/v2/jd/farm/read|http://106.13.212.194/jd/code/read/farm|" "$SCRIPTS_DIR/jd_fruit.js"
-    sed -i "s|http://jd.turinglabs.net/api/v2/jd/ddfactory/read|http://106.13.212.194/jd/code/read/ddfactory|" "$SCRIPTS_DIR/jd_jdfactory.js"
-    sed -i "s|http://jd.turinglabs.net/api/v2/jd/jxfactory/read|http://106.13.212.194/jd/code/read/jxfactory|" "$SCRIPTS_DIR/jd_dreamFactory.js"
-    sed -i "s|http://jd.turinglabs.net/api/v2/jd/pet/read|http://106.13.212.194/jd/code/read/pet|" "$SCRIPTS_DIR/jd_pet.js"
-    sed -i "s|http://jd.turinglabs.net/api/v2/jd/sgmh/read|http://106.13.212.194/jd/code/read/sgmh|" "$SCRIPTS_DIR/jd_sgmh.js"
-    sed -i "s|http://jd.turinglabs.net/api/v2/jd/bean/read|http://106.13.212.194/jd/code/read/bean|" "$SCRIPTS_DIR/jd_plantBean.js"
-    sed -i "s|https://code.chiang.fun/api/v1/jd/jdcrazyjoy/read|http://106.13.212.194/jd/code/read/crazyJoy|" "$SCRIPTS_DIR/jd_crazy_joy.js"
-    sed -i "s|https://code.chiang.fun/api/v1/jd/jdcash/read|http://106.13.212.194/jd/code/read/jdcash|" "$SCRIPTS_DIR/jd_cash.js"
-    echo "助力池链接替换完成!"
-}
+############################## 下载脚本 ##############################
+cd $ScriptsDir
+index=1
+for author in $author_list
+do
+  echo -e "############### 开始下载 $author 的脚本 ###############"
+  # 下载my_scripts_list中的每个js文件，重命名增加前缀"作者昵称_"，增加后缀".new"
+  eval scripts_list=\$my_scripts_list_${index}
+  eval url_list=\$scripts_base_url_${index}
+  for js in $scripts_list
+  do
+    eval url=$url_list$js
+    eval name=$author"_"$js
+    echo $name
+    wget -q --no-check-certificate $url -O $name.new
+
+    # 如果上一步下载没问题，才去掉后缀".new"，如果上一步下载有问题，就保留之前正常下载的版本
+    # 查找脚本内cron关键字并添加到crontab.list
+    if [ $? -eq 0 ]; then
+      mv -f $name.new $name
+      echo -e "更新 $name 完成...\n"
+	  croname=`echo "$name"|awk -F\. '{print $1}'`
+	  script_date=`cat  $name|grep "http"|awk '{if($1~/^[0-59]/) print $1,$2,$3,$4,$5}'|sort |uniq|head -n 1`
+	  if [ -z "${script_date}" ];then
+	    cron_min=$(rand 1 59)
+	    cron_hour=$(rand 7 9)
+	    [ $(grep -c "$croname" ${ConfigDir}/crontab.list) -eq 0 ] && sed -i "/hangup/a${cron_min} ${cron_hour} * * * bash jd $croname"  ${ConfigDir}/crontab.list
+	  else
+	    [ $(grep -c "$croname" ${ConfigDir}/crontab.list) -eq 0 ] && sed -i "/hangup/a${script_date} bash jd $croname"  ${ConfigDir}/crontab.list
+	  fi
+    else
+      [ -f $name.new ] && rm -f $name.new
+      echo -e "更新 $name 失败，使用上一次正常的版本...\n"
+    fi
+  done
+  index=$[$index+1]
+done
+
+############################## 更新群助力脚本 ##############################
+bash ${ConfigDir}/sharecode.sh
+
+############################## 更新diy.sh ##############################
+cd $ConfigDir
+echo -e "开始更新 diy.sh "
+wget -q --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/ljhnchina/jd_script/master/diy.sh -O diy.sh.new
+if [ $? -eq 0 ]; then
+  mv -f diy.sh.new diy.sh
+  echo -e "更新 diy.sh 完成"
+else
+  rm -rf diy.sh.new
+  echo -e "更新 diy.sh 失败，使用上一次正常的版本...\n"
+fi
 
 function updatePanel {
     cp -r $JD_DIR/scripts2/panel/* $JD_DIR/panel
