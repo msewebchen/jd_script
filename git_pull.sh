c3f267c241329faf9e44bfb489677b304ead6ac3
ljhnchina <jiandjh@qq.com>
2021-03-07 21:46:59 +0800

    Update git_pull.sh

--- git_pull.sh
+++ git_pull.sh
@@ -33,10 +33,10 @@ WhichDep=$(grep "/jd-base" "${ShellDir}/.git/config")
 Scripts2URL=https://github.com/ljhnchina/jd_script.git
 
 if [[ ${WhichDep} == *github* ]]; then
-  ScriptsURL=https://github.com/ljhnchina/jd_script.git
+  ScriptsURL=https://github.com/ljhnchina/lxk0301.git
   ShellURL=https://github.com/EvineDeng/jd-base
 else
-  ScriptsURL=https://github.com/ljhnchina/jd_script.git
+  ScriptsURL=https://github.com/ljhnchina/lxk0301.git
   ShellURL=https://gitee.com/evine/jd-base
 fi
 
