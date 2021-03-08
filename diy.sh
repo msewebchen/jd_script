ac94f96d4e75ad87ea20b3c0a95ae256a14a345e
ljhnchina <jiandjh@qq.com>
2021-03-08 07:44:32 -0600

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -32,7 +32,7 @@ my_scripts_list_2=""
 my_scripts_list_3="jd_entertainment.js jd_fanslove.js jd_gjmh.js jd_mlyjy.js jd_shake.js jd_shakeBean.js jd_xmf.js z_marketLottery.js"
 
 # 维护:whyour      库地址:https://github.com/whyour/hundun/tree/master/quanx
-my_scripts_list_4="didi.js"
+my_scripts_list_4=""
 
 # 维护:moposmall   库地址:https://github.com/moposmall/Script/tree/main/Me
 my_scripts_list_5="jx_cfd_exchange.js"
