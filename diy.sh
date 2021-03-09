3a6b21dfbba3ff502980224e17e41492e26b8d66
ljhnchina <jiandjh@qq.com>
2021-03-09 18:43:24 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # 把此diy.sh放入config即可,会自动同步最新脚本
-# 如有好用的脚本或者脚本更新不及时请@ljhncihna
-# 2021年3月9日 18:16:16
+# 如有好用的脚本或者脚本更新不及时请@qiao112
+# 2021年3月8日17:26
 
 ############################## 作者昵称 ##############################
 # 使用空格隔开
@@ -13,13 +13,13 @@ author_list="Tartarus2014 i-chenzhe whyour moposmall qq34347476 ZCY01 shuye72"
 # 1.从作者库中随意挑选一个脚本地址，每个作者的地址添加一个即可，无须重复添加
 # 2.将地址最后的 “脚本名称+后缀” 剪切到下一个变量里（my_scripts_list_xxx）
 scripts_base_url_1=https://ghproxy.com/https://raw.githubusercontent.com/ljhnchina/jd_script/master/
-scripts_base_url_2=https://ghproxy.com/https://raw.githubusercontent.com/Tartarus2014/Script/master/
-scripts_base_url_3=https://ghproxy.com/https://raw.githubusercontent.com/i-chenzhe/qx/main/
-scripts_base_url_4=https://ghproxy.com/https://raw.githubusercontent.com/whyour/hundun/master/quanx/
-scripts_base_url_5=https://ghproxy.com/https://raw.githubusercontent.com/moposmall/Script/main/Me/
-scripts_base_url_6=https://ghproxy.com/https://raw.githubusercontent.com/qq34347476/js_script/master/scripts/
-scripts_base_url_7=https://ghproxy.com/https://raw.githubusercontent.com/ZCY01/daily_scripts/main/jd/
-scripts_base_url_8=https://gitee.com/shuye72/MyActions/raw/main/
+scripts_base_url_1=https://ghproxy.com/https://raw.githubusercontent.com/Tartarus2014/Script/master/
+scripts_base_url_2=https://ghproxy.com/https://raw.githubusercontent.com/i-chenzhe/qx/main/
+scripts_base_url_3=https://ghproxy.com/https://raw.githubusercontent.com/whyour/hundun/master/quanx/
+scripts_base_url_4=https://ghproxy.com/https://raw.githubusercontent.com/moposmall/Script/main/Me/
+scripts_base_url_5=https://ghproxy.com/https://raw.githubusercontent.com/qq34347476/js_script/master/scripts/
+scripts_base_url_6=https://ghproxy.com/https://raw.githubusercontent.com/ZCY01/daily_scripts/main/jd/
+scripts_base_url_7=https://gitee.com/shuye72/MyActions/raw/main/
 
 ############################## 脚本名称 ##############################
 # 将相应作者的脚本填写到以下变量中
@@ -27,25 +27,25 @@ scripts_base_url_8=https://gitee.com/shuye72/MyActions/raw/main/
 my_scripts_list_1="diy_shopsign.js jd_crazy_joy_compose.js jd_firecrackers.js jd_submit_code.js jx_cfd.js jd_daydlt.js"
 
 # 维护:Tartarus2014 库地址:https://github.com/Tartarus2014/Script
-my_scripts_list_2=""
+my_scripts_list_1=""
 
 # 维护:i-chenzhe   库地址:https://github.com/i-chenzhe/qx
-my_scripts_list_3="jd_entertainment.js jd_fanslove.js jd_gjmh.js jd_mlyjy.js jd_shake.js jd_shakeBean.js jd_xmf.js z_marketLottery.js"
+my_scripts_list_2="jd_entertainment.js jd_fanslove.js jd_gjmh.js jd_shake.js jd_shakeBean.js jd_jump_jump.js z_marketLottery.js"
 
 # 维护:whyour      库地址:https://github.com/whyour/hundun/tree/master/quanx
-my_scripts_list_4=""
+my_scripts_list_3=""
 
 # 维护:moposmall   库地址:https://github.com/moposmall/Script/tree/main/Me
-my_scripts_list_5="jx_cfd_exchange.js"
+my_scripts_list_4="jx_cfd_exchange.js"
 
 # 维护:qq34347476  库地址:https://github.com/qq34347476/js_script
-my_scripts_list_6="format_share_jd_code.js"
+my_scripts_list_5="format_share_jd_code.js"
 
 # 维护:ZCY01       库地址:https://github.com/ZCY01/daily_scripts/tree/main/jd
-my_scripts_list_7="jd_priceProtect.js"
+my_scripts_list_6="jd_priceProtect.js"
 
 # 维护:shuye72     库地址:https://gitee.com/shuye72/MyActions/tree/main ps:尽量不要使用此人脚本,
-my_scripts_list_8=""
+my_scripts_list_7=""
 
 ############################## 随机函数 ##############################
 rand(){
